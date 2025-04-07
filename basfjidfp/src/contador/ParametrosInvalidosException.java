6101030e820958eb529d5d5d7c182af8b26896ba
David Vilas <133886340+celesteadoq@users.noreply.github.com>
2025-04-07 17:53:30 -0300

    Add files via upload

---
+++ basfjidfp/src/contador/ParametrosInvalidosException.java
@@ -0,0 +1,8 @@
+package contador;
+
+public class ParametrosInvalidosException extends Exception {
+
+	   public ParametrosInvalidosException() {
+	        super("O segundo parâmetro deve ser maior que o primeiro.");
+	    }
+}
