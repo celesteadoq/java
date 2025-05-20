af5394a4c057d8fa8b9e42e6f5b74100ede94efc
David Vilas <133886340+celesteadoq@users.noreply.github.com>
2025-05-20 17:21:03 -0300

    Delete directory

--- basfjidfp/src/contador/ParametrosInvalidosException.java
+++
@@ -1,8 +0,0 @@
-package contador;
-
-public class ParametrosInvalidosException extends Exception {
-
-	   public ParametrosInvalidosException() {
-	        super("O segundo parâmetro deve ser maior que o primeiro.");
-	    }
-}
